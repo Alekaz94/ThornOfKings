7d423e91b7e5bf2faf1dd7f6d2e696206620b589
alexandros <alexandros.kazalis@hotmail.com>
2026-04-09 12:42:32 +0200

    First Commit

---
+++ src/main/java/com/example/thornofkings/ThornOfKingsApplication.java
@@ -0,0 +1,13 @@
+package com.example.thornofkings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThornOfKingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThornOfKingsApplication.class, args);
+    }
+
+}
