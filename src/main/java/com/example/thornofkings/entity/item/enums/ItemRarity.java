70e58e1fd58332f4b0d6fbf49e9442df7788a79e
alexandros <alexandros.kazalis@hotmail.com>
2026-04-16 21:40:28 +0200

    Created Enum classes for Items. First for item rarity. Second for item slots.

---
+++ src/main/java/com/example/thornofkings/entity/item/enums/ItemRarity.java
@@ -0,0 +1,8 @@
+package com.example.thornofkings.entity.item.enums;
+
+public enum ItemRarity {
+    BASIC,
+    RARE,
+    EPIC,
+    LEGENDARY
+}
---
+++ src/main/java/com/example/thornofkings/entity/item/enums/ItemSlot.java
@@ -0,0 +1,14 @@
+package com.example.thornofkings.entity.item.enums;
+
+public enum ItemSlot {
+    HEAD,
+    NECKLACE,
+    CLOAK,
+    SHOULDER,
+    CHEST,
+    LEGS,
+    FEET,
+    RING_ONE,
+    RING_TWO,
+    BACKPACK
+}
