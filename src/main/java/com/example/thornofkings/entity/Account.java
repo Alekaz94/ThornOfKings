813f276f0c22e82087f0f4f5c949c966f6a18563
alexandros <alexandros.kazalis@hotmail.com>
2026-04-17 22:20:27 +0200

    Added getters, setter and constructors to Account.
    Changed field names for a few fields in Basestats and added 2 new fields.

--- src/main/java/com/example/thornofkings/entity/Account.java
+++ src/main/java/com/example/thornofkings/entity/Account.java
@@ -18,4 +18,63 @@ public class Account {
     private String name;
     private String email;
     private String passwordHash;
+
+    public Account() {
+    }
+
+    public Account(LocalDateTime createdAt, LocalDateTime updatedAt, String name, String email, String passwordHash) {
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.name = name;
+        this.email = email;
+        this.passwordHash = passwordHash;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
 }
--- src/main/java/com/example/thornofkings/entity/character/BaseStats.java
+++ src/main/java/com/example/thornofkings/entity/character/BaseStats.java
@@ -6,18 +6,20 @@ import jakarta.persistence.Embeddable;
 public class BaseStats {
     private int stamina;
     private int strength;
+    private int defence;
     private int agility;
-    private int intellect;
+    private int speed;
 
     public BaseStats() {
 
     }
 
-    public BaseStats(int stamina, int strength, int agility, int intellect) {
+    public BaseStats(int stamina, int strength, int defence, int agility, int speed) {
         this.stamina = stamina;
         this.strength = strength;
+        this.defence = defence;
         this.agility = agility;
-        this.intellect = intellect;
+        this.speed = speed;
     }
 
     public int getStamina() {
@@ -36,6 +38,14 @@ public class BaseStats {
         this.strength = strength;
     }
 
+    public int getDefence() {
+        return defence;
+    }
+
+    public void setDefence(int defence) {
+        this.defence = defence;
+    }
+
     public int getAgility() {
         return agility;
     }
@@ -44,11 +54,11 @@ public class BaseStats {
         this.agility = agility;
     }
 
-    public int getIntellect() {
-        return intellect;
+    public int getSpeed() {
+        return speed;
     }
 
-    public void setIntellect(int intellect) {
-        this.intellect = intellect;
+    public void setSpeed(int speed) {
+        this.speed = speed;
     }
 }
